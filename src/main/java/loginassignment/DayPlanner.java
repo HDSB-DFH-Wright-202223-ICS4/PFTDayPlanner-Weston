f3285674e0c9a53a1f5eeda95416a98263e08ca2
Weston Forbes <westonmichaelforbes@gmail.com>
2023-06-16 13:43:28 -0400

    Importance Column Added

--- src/main/java/loginassignment/DayPlanner.java
+++ src/main/java/loginassignment/DayPlanner.java
@@ -18,10 +18,21 @@ public class DayPlanner extends Application {
     @FXML private TextField stringTextField; //Used to grab the string from the text field.
     @FXML private DatePicker datePickerField;//Used to grab the date (converted to string) from text field.
 
+    @FXML private ChoiceBox importancePickerField; //Used to grab the selected importance (and to add the types to it)
+
+
     @FXML private TableView mainTableView; //Used to add new entries.
     @FXML private TableColumn textColumn; //Used to update the field's data.
     @FXML private TableColumn dateColumn; //Used to update the field's data as well.
+    @FXML private TableColumn importanceColumn; //Used to update the field's data also.
 
+
+    @FXML
+    public void initialize()
+    {//This is called when the FXML VBox is launched.
+        AddItems();
+        //DayPlannerSaveLoader.ReadDate();
+    }
 
     @FXML
     private void AddNewElement(ActionEvent event)
@@ -29,11 +40,13 @@ public class DayPlanner extends Application {
         event.consume();
         System.out.println(CanSubmitNewField());
         if(CanSubmitNewField() == true) { //If you meet the proper requirements to add to the list.
-            AddEntry(new DayPlannerItem(RetrieveText(), datePickerField.getValue().toString()));
+            AddEntry(new DayPlannerItem(RetrieveText(), datePickerField.getValue().toString(), importancePickerField.getSelectionModel().getSelectedItem().toString()));
+            System.out.println(importancePickerField.getSelectionModel().getSelectedItem().toString());
         }else
         {//Isn't catching the DatePicker being unchosen so there's an alternative try/catch that gets it down below.
-            ShowFieldsPopulatedAlert();
+            ShowAlert("All Fields Need To Be Populated!");
         }
+        //DayPlannerSaveLoader.WriteData();
     }
 
     @FXML
@@ -41,7 +54,10 @@ public class DayPlanner extends Application {
     {//Called directly by the remove button on the FXML, removes your current selected item.
         event.consume();
         //!!Maybe do a check here if getSelectedItem() != null, and else throw up error warning popup.
+        if(mainTableView.getSelectionModel().getSelectedItem() != null)
         mainTableView.getItems().removeAll(mainTableView.getSelectionModel().getSelectedItem());
+        else
+            ShowAlert("No Item Selected!");
     }
 
 
@@ -56,13 +72,23 @@ public class DayPlanner extends Application {
         Scene scene = new Scene(vbox);
         stage.setScene(scene);
         stage.show();
+
+        //DayPlanner.AddItems();
     }
+
+    public void AddItems()
+    {//This populates the Importance Picker Field (On Initialization)
+        importancePickerField.getItems().addAll("Not Important", "Barely Important", "Semi Important", "Pretty Important", "Important", "Very Important");
+        importancePickerField.getSelectionModel().select("Not Important");
+    }
+
 
     private void AddEntry(DayPlannerItem entry)
     { //Add new entry to the FXML UI component, then update the Cell's values to display correctly.
         mainTableView.getItems().add(entry);
         textColumn.setCellValueFactory(new PropertyValueFactory<>("text"));
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        importanceColumn.setCellValueFactory(new PropertyValueFactory<>("importance"));
     }
 
     private Boolean CanSubmitNewField()
@@ -75,24 +101,21 @@ public class DayPlanner extends Application {
             }
         }
         catch(Exception exc) { //Throw up the alert also over here (Turned the alert into a method to call because it's used more than once)
-            ShowFieldsPopulatedAlert();
+            ShowAlert("All Fields Need To Be Populated!");
         }
         return false; //In any other case, it'll be false as well.
     }
 
-    private void ShowFieldsPopulatedAlert()
+    private void ShowAlert(String text)
     {
-        Alert alert = new Alert(Alert.AlertType.WARNING, "All fields need to be populated.", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.WARNING, text, ButtonType.OK);
         alert.show();
     }
-
-
 
     private String RetrieveText()
     {//This will retrieve the text from the main input field.
         return stringTextField.getText();
     }
-
 
     @Override
     public void start(Stage stage) throws Exception {
--- src/main/java/loginassignment/DayPlannerItem.java
+++ src/main/java/loginassignment/DayPlannerItem.java
@@ -5,15 +5,18 @@ import java.util.Date;
 public class DayPlannerItem {
         public String text;
         public String date;
+        public String importance;
 
         public DayPlannerItem()
         {
             this.text = "";
             this.date = new String();
+            this.importance = new String();
         }
-        public DayPlannerItem(String Text, String Date) {
+        public DayPlannerItem(String Text, String Date, String Importance) {
             this.text = Text;
             this.date = Date;
+            this.importance = Importance;
         }
 
         public String getText()
@@ -35,6 +38,9 @@ public class DayPlannerItem {
         {
             this.date = date;
         }
+
+        public String getImportance() {return importance;}
+        public void setImportance(String importance) {this.importance = importance; }
 
         //Make new getters/setters for new variables, so they can be read from DayPlanner.java
 }
